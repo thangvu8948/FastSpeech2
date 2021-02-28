4f5252c5b74b6b0d7622079e7e85f47b172ad710
Đức Thắng <thangvu8948@gmail.com>
2021-03-01 04:06:32 +0700

    + generate_filelist

--- Generate_Filelist.py
+++ Generate_Filelist.py
@@ -2,12 +2,15 @@ import numpy as np
 import librosa
 import textgrid
 from utils.files import get_files
-def generate():
+import argparse
+import os
+def generate(parser):
     n_fft = 1024
     sample_rate = 22050
     hop_length = 256
     #a = get_files("D:\\DatasetTTS\\mfa\\montreal-forced-aligner\\outdir\\wavs", extension=".TextGrid")
-    a = get_files("D:\\FastSpeech2\\vivos\\out_vivos_f\\wavs_f", extension=".TextGrid")
+    #a = get_files("D:\\FastSpeech2\\vivos\\out_vivos_f\\wavs_f", extension=".TextGrid")
+    a = get_files(parser.tg_path, extension=".TextGrid")
     for i in range(0, len(a)):
         tgrid = textgrid.TextGrid.fromFile(a[i])
         time_list = []
@@ -38,14 +41,29 @@ def generate():
         Filelist_content = ' '.join([str(elem) for elem in words]) + "|" + ' '.join(
             [str(elem) for elem in frames]) + "|" + ' '.join([str(elem) for elem in diff_list]) + "|" + ' '.join(
             [str(elem) for elem in phonemes]) + "|" + file_name + "\n"
-        if (file_name[:8] in ["VIVOSDEV"]):
-            with open("filelists/" + "valid_filelist.txt", encoding="utf8", mode="a") as f:
+        # if (file_name[:8] in ["VIVOSDEV"]):
+        #     with open("filelists/" + "valid_filelist.txt", encoding="utf8", mode="a") as f:
+        #         f.write(Filelist_content)
+        # else:
+        #     with open("filelists/" + "train_filelist.txt", encoding="utf8", mode="a") as f1:
+        #         f1.write(Filelist_content)
+
+        #VTP
+        prefix = parser.prefix_valid
+        len_prefix = len(prefix)
+        if (file_name[:len_prefix] in [prefix]):
+            with open("filelists/" + parser.output_valid_name, encoding="utf8", mode="a") as f:
                 f.write(Filelist_content)
         else:
-            with open("filelists/" + "train_filelist.txt", encoding="utf8", mode="a") as f1:
+            with open("filelists/" + parser.output_train_name, encoding="utf8", mode="a") as f1:
                 f1.write(Filelist_content)
 
 def main():
-    generate()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tg_path', default=os.path.expanduser('"D:\\DatasetTTS\\mfa\\montreal-forced-aligner\\outtg_viet_tts\\wav"'))
+    parser.add_argument('--output_train_name', default='train_filelist2.txt')
+    parser.add_argument('--output_valid_name', default='valid_filelist2.txt')
+    parser.add_argument('--prefix_valid', default= "tts_lines_part_00")
+    generate(parser)
 if __name__ == "__main__":
     main()
